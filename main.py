5304e0c861da35217a44507468f2cc9b4242824a
Cipta Adinagara <ciptaadinagara@gmail.com>
2021-10-05 15:09:29 +0700

    main

---
+++ main.py
@@ -0,0 +1,19 @@
+def data_extract():
+    member = dict()
+    member['name'] = 'Michael'
+    member['dob'] = '19 October 1998'
+    member['address'] = 'Owl Street'
+    member['gender'] = 'Male'
+    return member
+    
+def data(result):
+    print(f"Name : {result['name']}")
+    print(f"Date of Birth : {result['dob']}")
+    print(f"Address : {result['address']}")
+    print(f"Gender : {result['gender']}")
+
+
+if __name__ == '__main__':
+    print("Main Program")
+    result = data_extract()
+    data(result)
